2d50d1510f5fb4490cb618bb9028286c38b6604f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-11 01:58:18 +0500

    still work
    continue

---
+++ app.js
@@ -0,0 +1,9 @@
+function randomjoke(){
+    let random = array["Mujha Kyun Nikala","Billo Rani", "Uffff....","123 four pilla","Ma Nahi Batao ga"]
+
+    let joke = document.getElementById("jokes");
+    let btns = document.getElementById("btn");
+
+    random = joke.innerText;
+    console.log(random) 
+}
